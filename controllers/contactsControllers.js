aa63f3994e77117f9c3f456e4c4338f49a6ec52d
Yurii Sykal <108358132+yuriiSY@users.noreply.github.com>
2024-03-18 10:26:45 +0000

    crud

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,72 @@
-import contactsService from "../services/contactsServices.js";
+import * as contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getAllContacts = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.getContactById(id);
 
-export const getOneContact = (req, res) => {};
+    if (!result) {
+      throw HttpError(404, `Contact with ${id} not found`);
+    }
 
-export const deleteContact = (req, res) => {};
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
 
-export const updateContact = (req, res) => {};
+    if (!result) {
+      throw HttpError(404, `Contact with ${id} not found`);
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  try {
+    const body = req.body;
+    // const { error } = createContactSchema.validate(body);
+    // if (error) {
+    //   throw HttpError(400, error.message);
+    // }
+    const result = await contactsService.addContact(body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const body = req.body;
+    const { id } = req.params;
+    const result = await contactsService.updateContactById(id, body);
+    if (!result) {
+      throw HttpError(404, `Contact with ${id} not found`);
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
